c278b4c8473faf90afa49848cdd5c3faa060dee1
Brian Hoffman <brianjhoffman@gmail.com>
2012-08-11 13:47:01 -0400

    changed archival object id

--- common/schemas/archival_object.rb
+++ common/schemas/archival_object.rb
@@ -4,14 +4,10 @@
     "uri" => "/repositories/:repo_id/archival_objects",
     "properties" => {
       "uri" => {"type" => "string", "required" => false},
-
-      "id_0" => {"type" => "string", "ifmissing" => "error", "minLength" => 1, "pattern" => "^[a-zA-Z0-9]*$"},
-      "id_1" => {"type" => "string", "required" => false, "default" => "", "pattern" => "^[a-zA-Z0-9]*$"},
-      "id_2" => {"type" => "string", "required" => false, "default" => "", "pattern" => "^[a-zA-Z0-9]*$"},
-      "id_3" => {"type" => "string", "required" => false, "default" => "", "pattern" => "^[a-zA-Z0-9]*$"},
-
+      "ref_id" => {"type" => "string", "ifmissing" => "error", "minLength" => 1, "pattern" => "^[a-zA-Z0-9]*$"},
+      "component_id" => {"type" => "string", "required" => false, "default" => "", "pattern" => "^[a-zA-Z0-9]*$"},
       "title" => {"type" => "string", "minLength" => 1, "required" => true},
-
+      "level" => {"type" => "string", "minLength" => 1, "required" => false},
       "parent" => {"type" => "string", "required" => false, "pattern" => "/repositories/[0-9]+/archival_objects/[0-9]+$"},
       "collection" => {"type" => "string", "required" => false, "pattern" => "/repositories/[0-9]+/collections/[0-9]+$"},
     },
