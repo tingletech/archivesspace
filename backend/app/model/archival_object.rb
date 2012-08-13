74d9f5eda3e1610eeb6e3dbcf2cfa18c4f4c9f93
Brian Hoffman <brianjhoffman@gmail.com>
2012-08-13 14:10:59 +1000

    Removed id_0, id_1, id_2, and id_4 from archival_object table, model, controllers, and schema
    Added ref_id and component_id (the second is just a placeholder for now)
    
    Added uniqueness constraint for ref_id within a collection
    
    Added test to ensure that a duplicate ref_id throws an exception

--- backend/app/model/archival_object.rb
+++ backend/app/model/archival_object.rb
@@ -1,7 +1,6 @@
 class ArchivalObject < Sequel::Model(:archival_objects)
   plugin :validation_helpers
   include ASModel
-  include Identifiers
 
   many_to_many :subjects
 
@@ -58,6 +57,13 @@ class ArchivalObject < Sequel::Model(:archival_objects)
     json = super(obj, type)
     json.subjects = obj.subjects.map {|subject| JSONModel(:subject).uri_for(subject.id)}
     json
+  end
+
+
+  def validate
+    validates_unique([:collection_id, :ref_id],
+                     :message => "An Archival Object Ref ID must be unique to its collection")
+    super
   end
 
 end
